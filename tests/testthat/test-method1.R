7691f8ad780e4ed293d4b077b345462939aa44e4
Carson Zhang <carsonzhang4@gmail.com>
2025-08-09 15:20:48 +0200

    data

--- tests/testthat/test-method1.R
+++ tests/testthat/test-method1.R
@@ -1,7 +1,13 @@
 test_that("subgrouping works on the simple example from the article", {
-  
+  subgroups_res = method1(ghosh_table_3$D_i)
+
+  expect_equal(subgroups_res$subgroups_idx, ghosh_table_3$True_Subgroup)
 })
 
 test_that("subgrouping works when there are ties", {
+  subgroups_res = method1(heartburn_multi_center$D)
 
+  # TODO: compute the true subgroups
+
+  # expect_equal(subgroups_res$subgroups_idx, ghosh_table_3$True_Subgroup)
 })
